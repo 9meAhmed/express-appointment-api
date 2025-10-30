3df40a08fb831989ee017cdfb5c8230810b91397
Noman Ahmed <noman.ahmed1408@gmail.com>
2025-10-30 22:40:22 +0500

    profile route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import { AuthController } from "../controllers/auth.controller";
 import { userValidator, loginValidator } from "../middleware/index";
 import { resetPasswordValidator } from "../middleware/reset-password.validator";
+import { authentification } from "../middleware/authentification";
 const Router = express.Router();
 
 Router.post("/login", loginValidator, AuthController.loginUser);
@@ -14,5 +15,6 @@ Router.post(
   resetPasswordValidator,
   AuthController.resetPassword
 );
+Router.get("/profile", authentification, AuthController.profile);
 
 export { Router as authRouter };
