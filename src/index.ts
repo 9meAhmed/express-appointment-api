aed59556994c59683fd33c849d3acf55688d9814
Noman Ahmed <noman.ahmed1408@gmail.com>
2025-11-10 13:40:19 +0500

    disabled some routes

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", authRouter);
-app.use("/api", userRouter);
-app.use("/api", appointmentRouter);
+// app.use("/api", userRouter);
+// app.use("/api", appointmentRouter);
 
 app.use(errorHandler);
 
