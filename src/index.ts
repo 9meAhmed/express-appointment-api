8930b246877c3c6e82e7909199aaa75fd144ef04
Noman Ahmed <noman.ahmed1408@gmail.com>
2025-11-11 08:58:34 +0500

    vercel await for routes to be loaded

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express from "express";
 import cors from "cors";
 import { AppDataSource } from "./data-source";
 import errorHandler from "./middleware/errorHandler";
-import { userRouter } from "./routes/user.routes";
-import { authRouter } from "./routes/auth.routes";
-import { appointmentRouter } from "./routes/appointment.routes";
+// import { userRouter } from "./routes/user.routes";
+// import { authRouter } from "./routes/auth.routes";
+// import { appointmentRouter } from "./routes/appointment.routes";
 import cookieParser from "cookie-parser";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
@@ -22,13 +22,15 @@ app.use(express.static("uploads/"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", authRouter);
-app.use("/api", userRouter);
-app.use("/api", appointmentRouter);
+// app.use("/api", authRouter);
+// app.use("/api", userRouter);
+// app.use("/api", appointmentRouter);
 
 app.use(errorHandler);
 
 let isDataSourceInitialized = false;
+let areRoutesMounted = false;
+
 async function initDataSource() {
   if (!isDataSourceInitialized) {
     try {
@@ -43,5 +45,17 @@ async function initDataSource() {
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   await initDataSource();
+
+  if (!areRoutesMounted) {
+    const { authRouter } = await import("./routes/auth.routes");
+    const { userRouter } = await import("./routes/user.routes");
+    const { appointmentRouter } = await import("./routes/appointment.routes");
+
+    app.use("/api", authRouter);
+    app.use("/api", userRouter);
+    app.use("/api", appointmentRouter);
+
+    areRoutesMounted = true;
+  }
   app(req, res);
 }
